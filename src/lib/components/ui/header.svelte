0baad2df7d02800fcec56fb450f91662e161c9a8
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2023-11-10 20:37:11 -0300

    Added header

---
+++ src/lib/components/ui/header.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	const navItems: Array<{ name: string; href: string }> = [
+		{ name: 'inicio', href: '/' },
+		{ name: 'environment', href: '/environment' },
+		{ name: 'configuration', href: '/configuration' }
+	];
+</script>
+
+<header class="w-full h-20 flex justify-around items-center bg-primary-50 shadow z-10">
+	<span
+		class="text-3xl font-medium hover:text-primary-600 focus-within:text-primary-600 hover:underline focus-within:underline underline-offset-2 transition-colors duration-300"
+	>
+		<a class="block" href="/">DEMPS</a>
+	</span>
+	<nav>
+		<ul class="flex items-center gap-x-6">
+			{#each navItems as { name, href }}
+				<li
+					class="hover:text-primary-600 focus-within:text-primary-600 hover:underline focus-within:underline underline-offset-2 transition-colors duration-300"
+				>
+					<a class="w-full block" {href}>{name}</a>
+				</li>
+			{/each}
+			<li>
+				<button
+					class="bg-primary-600 px-6 py-2 text-white rounded-md hover:bg-primary-800 focus-visible:bg-primary-800 transition-colors duration-300"
+					>Download GeoJSON</button
+				>
+			</li>
+			<li>
+				<button
+					class="bg-primary-600 px-6 py-2 text-white rounded-md hover:bg-primary-800 focus-visible:bg-primary-800 transition-colors duration-300"
+				>
+					Run
+				</button>
+			</li>
+		</ul>
+	</nav>
+</header>
---
+++ src/routes/(app)/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { Header } from '$lib/components/ui';
+</script>
+
+<div class="w-full h-full grid grid-rows-[auto_1fr]">
+	<Header />
+
+	<main>
+		<slot />
+	</main>
+</div>
