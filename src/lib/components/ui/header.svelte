0c481d133201479fc0262681ef6927c3244fee0d
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2023-11-12 01:53:02 -0300

    Added download GeoJSON feature

--- src/lib/components/ui/header.svelte
+++ src/lib/components/ui/header.svelte
@@ -1,9 +1,25 @@
 <script lang="ts">
+	import { get } from 'svelte/store';
+	import { GeoJSONStore } from '$lib/stores';
+
 	const navItems: Array<{ name: string; href: string }> = [
 		{ name: 'inicio', href: '/' },
 		{ name: 'environment', href: '/environment' },
 		{ name: 'configuration', href: '/configuration' }
 	];
+
+	let URLdata: any;
+
+	const downloadGeoJSON = () => {
+		const data = get(GeoJSONStore);
+
+		if (data.features.length > 0) {
+			const convertedData = 'text/json;charset=utf-8,' + encodeURIComponent(JSON.stringify(data));
+			URLdata = 'data:' + convertedData;
+		} else {
+			// alert('El mapa no tiene datos para exportar');
+		}
+	};
 </script>
 
 <header class="w-full h-20 flex justify-around items-center bg-primary-50 shadow z-10">
@@ -22,9 +38,12 @@
 				</li>
 			{/each}
 			<li>
-				<button
+				<a
+					href={URLdata}
+					download={'data.geojson'}
+					on:click={downloadGeoJSON}
 					class="bg-primary-600 px-6 py-2 text-white rounded-md hover:bg-primary-800 focus-visible:bg-primary-800 transition-colors duration-300"
-					>Download GeoJSON</button
+					>Download GeoJSON</a
 				>
 			</li>
 			<li>
