6a1aadf561d07b23faf338ce4461e61a2172c83b
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2023-11-12 18:20:28 -0300

    Added leaflet draw and drag features

--- src/lib/components/leaflet/leaflet.svelte
+++ src/lib/components/leaflet/leaflet.svelte
@@ -1,16 +1,24 @@
 <script lang="ts">
 	import { onMount, setContext } from 'svelte';
-	import type L from 'leaflet';
 	import { key } from './key';
 
+	import type L from 'leaflet';
+	import type D from 'leaflet-draw';
+
 	import 'leaflet/dist/leaflet.css';
+	import 'leaflet-draw/dist/leaflet.draw.css';
 
 	export let view: L.LatLngExpression;
 	export let zoom: number;
 
 	let leaflet: typeof L;
-	let mapContainer: HTMLDivElement;
+	let leaflet_draw: typeof D;
+	let leaflet_path_drag: any;
+
 	let map: L.Map;
+	let draw: L.Control.Draw;
+	let features: L.FeatureGroup;
+	let mapContainer: HTMLDivElement;
 
 	setContext(key, {
 		getLeaflet: () => leaflet,
@@ -19,8 +27,51 @@
 
 	onMount(async () => {
 		leaflet = await import('leaflet');
+		leaflet_draw = await import('leaflet-draw');
+		// @ts-expect-error - no types for leaflet-path-drag
+		leaflet_path_drag = await import('leaflet-path-drag');
+
 		map = leaflet.map(mapContainer).setView(view, zoom);
 		leaflet.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+		features = new leaflet.FeatureGroup().addTo(map);
+		draw = new leaflet.Control.Draw({
+			draw: {
+				marker: false,
+				polyline: false,
+				rectangle: false,
+				circlemarker: false
+			},
+			edit: {
+				featureGroup: features
+			}
+		}).addTo(map);
+
+		map.on('draw:created', (e: L.LeafletEvent) => {
+			const layer = e.layer;
+
+			if (layer instanceof leaflet.Polygon) {
+				// @ts-expect-error - bad types for leaflet
+				const coordinates = [...layer._latlngs][0];
+				const polygon = new leaflet.Polygon([coordinates], {
+					// @ts-expect-error - no types for leaflet-path-drag
+					draggable: true
+				});
+
+				features.addLayer(polygon);
+			}
+			if (layer instanceof leaflet.Circle) {
+				// @ts-expect-error - bad types for leaflet
+				const coordinates = layer._latlng;
+				// @ts-expect-error - bad types for leaflet
+				const radius = layer._mRadius;
+				const circle = new leaflet.Circle(coordinates, radius, {
+					// @ts-expect-error - no types for leaflet-path-drag
+					draggable: true
+				});
+
+				features.addLayer(circle);
+			}
+		});
 	});
 </script>
 
@@ -29,3 +80,9 @@
 		<slot />
 	{/if}
 </div>
+
+<style lang="postcss">
+	:global(.leaflet-marker-icon) {
+		@apply rounded-full;
+	}
+</style>
