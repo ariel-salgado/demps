1034fd03fe71ef3b3791a66bdd6f28cead22b4d6
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2023-11-13 23:43:47 -0300

    Added main aside features

--- src/routes/(app)/configuration/+page.svelte
+++ src/routes/(app)/configuration/+page.svelte
@@ -1,25 +1,32 @@
 <script lang="ts">
+	let selected: number | null = null;
+
 	const asideItems: Array<{ title: string; href: string }> = [
 		{ title: 'Hola', href: '#section-1' },
 		{ title: 'Como', href: '#section-2' },
 		{ title: 'estas?', href: '#section-3' }
 	];
+
+	const handleSelected = (index: number) => {
+		selected = index;
+	};
 </script>
 
 <div class="w-full h-full grid grid-cols-4">
-	<aside class="grid grid-rows-[auto_auto_1fr] gap-y-4 col-span-1 p-8 shadow-md bg-primary-50">
+	<aside class="grid grid-rows-[auto_auto_1fr] gap-y-3 col-span-1 p-8 shadow-md bg-primary-50">
 		<span class="text-center">
-			<h2 class="text-2xl font-medium">Configuration</h2>
+			<h2 class="text-3xl font-medium">Configuration</h2>
 		</span>
 
 		<hr />
 
-		<ul class="h-min flex flex-col border-l-4 border-primary-400">
-			{#each asideItems as { title, href }}
+		<ul class="h-min flex flex-col py-4">
+			{#each asideItems as { title, href }, index}
 				<li
-					class="w-full text-primary-900 hover:bg-primary-600 focus-within:bg-primary-600 hover:text-white focus-within:text-white transition-colors duration-300 rounded-r-md"
+					class="w-full text-primary-900 border-l-4 border-primary-200 hover:bg-primary-400 focus-within:bg-primary-400 hover:border-primary-600 focus-within:border-primary-600 hover:text-white focus-within:text-white transition-colors duration-300 rounded-r-md"
+					class:selected={index === selected}
 				>
-					<a class="block px-3 py-1.5" {href}>{title}</a>
+					<a class="block px-3 py-2" {href} on:click={() => handleSelected(index)}>{title}</a>
 				</li>
 			{/each}
 		</ul>
@@ -47,3 +54,9 @@
 		</form>
 	</div>
 </div>
+
+<style lang="postcss">
+	.selected {
+		@apply bg-primary-600 border-primary-600 text-white;
+	}
+</style>
