7295d3c27851dc6a545cbc2faf3228499bf661d5
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2023-11-14 21:33:00 -0300

    Composable elements

--- src/lib/components/ui/form/index.ts
+++ src/lib/components/ui/form/index.ts
@@ -1,7 +1,8 @@
+import FormTitle from './form-title.svelte';
 import FormGroup from './form-group.svelte';
 import Input from './input.svelte';
 import Select from './select.svelte';
 import Label from './label.svelte';
 import Hint from './hint.svelte';
 
-export { FormGroup, Input, Select, Label, Hint };
+export { FormTitle, FormGroup, Input, Select, Label, Hint };
