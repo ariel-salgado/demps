0500a537e44e9a7eb21bf8ccd1039da858334b5d
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2023-11-16 19:35:39 -0300

    Added ViewTransition API

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,19 @@
 <script lang="ts">
 	import '../app.postcss';
+	import { onNavigate } from '$app/navigation';
+
+	onNavigate((navigation) => {
+		// @ts-expect-error - ViewTransition API is not in the types
+		if (!document.startViewTransition) return;
+
+		return new Promise((resolve) => {
+			// @ts-expect-error - ViewTransition API is not in the types
+			document.startViewTransition(async () => {
+				resolve();
+				await navigation.complete;
+			});
+		});
+	});
 </script>
 
 <slot />
