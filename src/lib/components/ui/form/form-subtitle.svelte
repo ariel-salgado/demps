7f4742ded905f4190944c26043a9c06fab1ce9eb
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2023-11-17 22:55:11 -0300

    Added subtitle for forms

---
+++ src/lib/components/ui/form/form-subtitle.svelte
@@ -0,0 +1,16 @@
+<div {...$$restProps}>
+	<h3>
+		<slot />
+	</h3>
+	<hr />
+</div>
+
+<style lang="postcss">
+	div {
+		@apply px-4 py-3;
+	}
+
+	h3 {
+		@apply text-2xl font-medium p-1;
+	}
+</style>
--- src/lib/components/ui/form/index.ts
+++ src/lib/components/ui/form/index.ts
@@ -1,8 +1,9 @@
 import FormTitle from './form-title.svelte';
+import FormSubtitle from './form-subtitle.svelte';
 import FormGroup from './form-group.svelte';
 import Input from './input.svelte';
 import Select from './select.svelte';
 import Label from './label.svelte';
 import Hint from './hint.svelte';
 
-export { FormTitle, FormGroup, Input, Select, Label, Hint };
+export { FormTitle, FormSubtitle, FormGroup, Input, Select, Label, Hint };
