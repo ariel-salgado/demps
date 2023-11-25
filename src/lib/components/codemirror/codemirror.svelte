893eeb6022312ea92ddc6697a3cc8a05ab82e80f
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2023-11-25 03:55:40 -0300

    Handling changes in the editor

--- src/lib/components/codemirror/codemirror.svelte
+++ src/lib/components/codemirror/codemirror.svelte
@@ -1,57 +1,65 @@
 <script lang="ts">
+	import { get } from 'svelte/store';
 	import { setContext } from 'svelte';
 	import type { Action } from 'svelte/action';
+
 	import { EditorView } from '@codemirror/view';
-	import { stringify } from '$lib/utils/helpers';
 	import { EditorState } from '@codemirror/state';
 	import { key } from '$lib/components/codemirror';
 	import { extensions } from '$lib/components/codemirror';
 
-	interface Props {
-		source: GeoJSON.FeatureCollection;
-	}
+	import { EnvStore } from '$lib/stores';
+	import { isValidGeoJSON, stringify } from '$lib/utils/helpers';
 
-	let { source } = $props<Props>();
 	let editor: EditorView | undefined = $state(undefined);
 
 	setContext(key, {
 		getEditor: () => editor
 	});
 
-	const initEditor: Action<HTMLDivElement, GeoJSON.FeatureCollection> = (
-		editorContainer: HTMLDivElement,
-		source: GeoJSON.FeatureCollection
-	) => {
+	const initEditor: Action<HTMLDivElement> = (editorContainer: HTMLDivElement) => {
 		editor = new EditorView({
 			parent: editorContainer,
 			state: EditorState.create({
-				doc: stringify(source),
-				extensions: [...extensions]
+				doc: stringify(get(EnvStore).data),
+				extensions: [
+					...extensions,
+					EditorView.updateListener.of((update) => {
+						if (update.docChanged) {
+							const value = update.state.doc.toString();
+							if (isValidGeoJSON(value)) {
+								EnvStore.set({ data: JSON.parse(value), trigger: 'editor' });
+							}
+						}
+					})
+				]
 			})
 		});
 
 		return {
-			update: (source: GeoJSON.FeatureCollection) => {
-				if (editor) {
-					editor?.dispatch({
-						changes: {
-							from: 0,
-							to: editor.state.doc.length,
-							insert: stringify(source)
-						}
-					});
-				}
-			},
 			destroy: () => {
 				editor?.destroy();
 				editor = undefined;
 			}
 		};
 	};
+
+	// Updates the editor only if the changes are not triggered by the editor itself
+	EnvStore.subscribe((update) => {
+		if (editor && update.trigger !== 'editor') {
+			editor.dispatch({
+				changes: {
+					from: 0,
+					to: editor.state.doc.length,
+					insert: stringify(update.data)
+				}
+			});
+		}
+	});
 </script>
 
 <div class="h-full grid grid-rows-[1fr_auto]">
-	<div class="relative overflow-y-scroll text-sm" use:initEditor={source}>
+	<div class="relative overflow-y-scroll text-sm" use:initEditor>
 		{#if editor}
 			<slot name="widgets" />
 		{/if}
