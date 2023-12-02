c359cad8c45dab578f788ad03ae6896f8cb88d10
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2023-12-02 18:34:29 -0300

    Added landing page

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -4,5 +4,9 @@
 
 <SEO title="DEMPS" description="DEMPS" />
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section class="grid h-full w-full place-items-center items-center">
+	<div class="mb-20 text-center">
+		<h1 class="text-6xl font-medium">DEMPS</h1>
+		<p class="text-lg italic text-slate-500">Simulador de evacuaciones masivas.</p>
+	</div>
+</section>
