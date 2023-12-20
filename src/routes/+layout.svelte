d03aaa50b37446fbd9e47cad6fb43e80f0bb2468
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2023-12-20 20:18:55 -0300

    Added view transition

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,21 @@
 <script lang="ts">
 	import '../app.pcss';
+	import { onNavigate } from '$app/navigation';
 
 	let { children } = $props();
+
+	onNavigate((navigation) => {
+		// @ts-ignore - startViewTransition is not in the types
+		if (!document.startViewTransition) return;
+
+		return new Promise((resolve) => {
+			// @ts-ignore - startViewTransition is not in the types
+			document.startViewTransition(async () => {
+				resolve();
+				await navigation.complete;
+			});
+		});
+	});
 </script>
 
 {@render children()}
