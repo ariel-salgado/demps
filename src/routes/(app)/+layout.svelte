7df26cedbb6ce9065482746d386dcb20cc6d9fcd
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2023-12-20 20:19:07 -0300

    Added main layout

---
+++ src/routes/(app)/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { Header } from '$lib/components/ui/header';
+
+	let { children } = $props();
+</script>
+
+<div class="grid h-full grid-rows-[auto_1fr_auto]">
+	<Header></Header>
+
+	<main>
+		{@render children()}
+	</main>
+
+	<footer></footer>
+</div>
