b91fe20c720dddeef75d339b2c882f989cbb418a
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2023-12-20 20:15:02 -0300

    Added snippets and dynamic height

--- src/lib/components/codemirror/editor.svelte
+++ src/lib/components/codemirror/editor.svelte
@@ -1,34 +1,37 @@
-<script context="module">
-	export const key = Symbol();
-</script>
-
 <script lang="ts">
 	import type { Snippet } from 'svelte';
 	import type { Action } from 'svelte/action';
 	import type { HTMLAttributes } from 'svelte/elements';
 
+	import { cn } from '$lib/utils';
 	import { setContext } from 'svelte';
 	import { EditorView } from '@codemirror/view';
 	import { EditorState } from '@codemirror/state';
+	import { key } from '$lib/components/codemirror';
 	import { extensions } from '$lib/components/codemirror/extensions';
 
 	interface Props extends HTMLAttributes<HTMLDivElement> {
-		children?: Snippet;
+		widgets?: Snippet<void>;
+		actions?: Snippet<void>;
 	}
 
-	let { children, ...props } = $props<Props>();
+	let { widgets, actions, class: className, ...props } = $props<Props>();
 
 	let editor: EditorView | undefined = $state(undefined);
+	let topPosition: number | undefined = $state(undefined);
+	let innerHeight: number | undefined = $state(undefined);
 
 	setContext(key, {
 		getEditor: () => editor
 	});
 
 	const initEditor: Action<HTMLDivElement> = (editorContainer: HTMLDivElement) => {
+		topPosition = editorContainer.getBoundingClientRect().top;
+
 		editor = new EditorView({
 			parent: editorContainer,
 			state: EditorState.create({
-				doc: 'Hello World',
+				doc: JSON.stringify({ type: 'FeatureCollection', features: [] }, null, 2),
 				extensions: [extensions]
 			})
 		});
@@ -42,8 +45,24 @@
 	};
 </script>
 
-<div use:initEditor {...props}>
-	{#if editor && children}
-		{@render children()}
+<svelte:window bind:innerHeight />
+
+<div
+	class={cn('flex h-full w-full flex-col overflow-y-auto', className)}
+	style="max-height: {innerHeight! - topPosition!}px"
+	{...props}
+>
+	<div class="h-full w-full" use:initEditor>
+		{#if editor && widgets}
+			{@render widgets()}
+		{/if}
+	</div>
+
+	{#if editor}
+		<div class="rounded-t-md border-x border-t border-x-slate-300 border-t-slate-300 p-6">
+			{#if editor && actions}
+				{@render actions()}
+			{/if}
+		</div>
 	{/if}
 </div>
