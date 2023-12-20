3724c1d1f43e2351cc2c0707e16bc0de61059f3a
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2023-12-20 20:16:11 -0300

    Added widgets to editor

---
+++ src/lib/components/codemirror/widgets.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import type { HTMLAttributes } from 'svelte/elements';
+
+	interface Props extends HTMLAttributes<HTMLDivElement> {
+		children: Snippet<void>;
+	}
+
+	let { children, ...props } = $props<Props>();
+</script>
+
+<div class="sticky top-6 z-10 float-right mr-14 h-0 w-0" {...props}>
+	<div class="grid gap-y-4">
+		{@render children()}
+	</div>
+</div>
