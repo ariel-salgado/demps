4c9d8f5b2d83b2f48e2a631201d4e72d3f01630c
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2023-12-20 20:17:03 -0300

    Added icons

---
+++ src/lib/components/icons/index.ts
@@ -0,0 +1,11 @@
+import Play from './play.svelte';
+import Clipboard from './clipboard.svelte';
+import ClipboardChecked from './clipboard-checked.svelte';
+import Download from './download.svelte';
+
+export { 
+    Play as PlayIcon,
+    Clipboard as ClipboardIcon,
+    ClipboardChecked as ClipboardCheckedIcon,
+    Download as DownloadIcon
+}
