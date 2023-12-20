1e23b05364ec48452afa20f01531594fe82f2f48
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2023-12-20 20:19:25 -0300

    Added environment route

---
+++ src/routes/(app)/environment/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+	import { Editor, Widgets, Clipboard, Download } from '$lib/components/codemirror';
+</script>
+
+<Editor>
+	{#snippet widgets()}
+		<Widgets>
+			<Clipboard />
+			<Download />
+		</Widgets>
+	{/snippet}
+	{#snippet actions()}
+		<Button>Button</Button>
+	{/snippet}
+</Editor>
