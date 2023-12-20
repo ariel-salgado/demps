7427a035fbe81d1c6cdfdce43d69cef8cd2d4ed2
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2023-12-20 20:18:03 -0300

    Added widgets

--- src/lib/components/codemirror/index.ts
+++ src/lib/components/codemirror/index.ts
@@ -1,5 +1,19 @@
+import type { EditorView } from '@codemirror/view';
+
+import { key } from './context'
 import Editor from './editor.svelte';
+import Widgets from './widgets.svelte';
+import Clipboard from './clipboard.svelte';
+import Download from './download.svelte';
+
+export type EditorContext = {
+	getEditor: () => EditorView;
+};
 
 export {
-    Editor
+    key,
+    Editor,
+    Widgets,
+    Clipboard,
+    Download
 }
