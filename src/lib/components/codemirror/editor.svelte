591c81c43ab565671d6f87292f28b0aa1fc41983
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2023-12-21 02:29:39 -0300

    Added editor handlers

--- src/lib/components/codemirror/editor.svelte
+++ src/lib/components/codemirror/editor.svelte
@@ -1,42 +1,76 @@
 <script lang="ts">
 	import type { Snippet } from 'svelte';
 	import type { Action } from 'svelte/action';
+	import type { GeoJSONStore } from '$lib/stores';
+	import type { FeatureCollection } from 'geojson';
+	import type { ViewUpdate } from '@codemirror/view';
 	import type { HTMLAttributes } from 'svelte/elements';
 
-	import { cn } from '$lib/utils';
 	import { setContext } from 'svelte';
 	import { EditorView } from '@codemirror/view';
 	import { EditorState } from '@codemirror/state';
 	import { key } from '$lib/components/codemirror';
+	import { cn, debounce, strEqualsObj } from '$lib/utils';
 	import { extensions } from '$lib/components/codemirror/extensions';
 
 	interface Props extends HTMLAttributes<HTMLDivElement> {
 		widgets?: Snippet<void>;
 		actions?: Snippet<void>;
+		data: GeoJSONStore;
 	}
 
-	let { widgets, actions, class: className, ...props } = $props<Props>();
+	let { widgets, actions, data, class: className, ...props } = $props<Props>();
 
-	let editor: EditorView | undefined = $state(undefined);
-	let topPosition: number | undefined = $state(undefined);
-	let innerHeight: number | undefined = $state(undefined);
+	let editor: EditorView | undefined = $state();
+	let topPosition: number | undefined = $state();
+	let innerHeight: number | undefined = $state();
 
 	setContext(key, {
 		getEditor: () => editor
 	});
 
-	const initEditor: Action<HTMLDivElement> = (editorContainer: HTMLDivElement) => {
+	const updateStore = debounce((value: string) => {
+		data.set(JSON.parse(value));
+	}, 750);
+
+	const editorOnChange = EditorView.updateListener.of((v: ViewUpdate) => {
+		const value = v.state.doc.toString();
+		if (v.docChanged && !strEqualsObj(value, $data)) {
+			updateStore(value);
+		}
+	});
+
+	const updateEditor = (value: FeatureCollection) => {
+		if (!strEqualsObj(editor!.state.doc.toString(), value)) {
+			editor?.dispatch({
+				changes: {
+					from: 0,
+					to: editor.state.doc.length,
+					insert: JSON.stringify(value, null, 2)
+				}
+			});
+		}
+	};
+
+	const initEditor: Action<HTMLDivElement, FeatureCollection> = (
+		editorContainer: HTMLDivElement,
+		features: FeatureCollection
+	) => {
 		topPosition = editorContainer.getBoundingClientRect().top;
 
 		editor = new EditorView({
 			parent: editorContainer,
 			state: EditorState.create({
-				doc: JSON.stringify({ type: 'FeatureCollection', features: [] }, null, 2),
-				extensions: [extensions]
+				doc: JSON.stringify(features, null, 2),
+				extensions: [extensions, editorOnChange]
 			})
 		});
 
 		return {
+			update(value: FeatureCollection) {
+				updateEditor(value);
+			},
+
 			destroy() {
 				editor?.destroy();
 				editor = undefined;
@@ -52,7 +86,7 @@
 	style="max-height: {innerHeight! - topPosition!}px"
 	{...props}
 >
-	<div class="h-full w-full" use:initEditor>
+	<div class="h-full w-full" use:initEditor={$data}>
 		{#if editor && widgets}
 			{@render widgets()}
 		{/if}
