169780b18f37da041b89845840945877bff807a4
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2023-12-21 20:58:46 -0300

    Added preprocessing on file upload

--- src/routes/(app)/environment/+page.svelte
+++ src/routes/(app)/environment/+page.svelte
@@ -1,7 +1,36 @@
 <script lang="ts">
+	import type { FeatureCollection, Feature } from 'geojson';
+
 	import { envStore } from '$lib/stores';
-	import { Button } from '$lib/components/ui/button';
+	import { UploadIcon } from '$lib/components/icons';
+	import { Fileupload } from '$lib/components/ui/forms';
+	import { isValidGeoJSON, preprocessGeoJSON } from '$lib/utils';
 	import { Editor, Widgets, Clipboard, Download } from '$lib/components/codemirror';
+
+	let files: FileList | null = $state(null);
+
+	const handleUpload = () => {
+		if (!!files && files.length > 0) {
+			const reader = new FileReader();
+
+			reader.onload = () => {
+				const uploadedData = reader.result as string;
+
+				if (!isValidGeoJSON(uploadedData)) {
+					alert('Invalid GeoJSON');
+					return;
+				}
+
+				const geojson = preprocessGeoJSON(JSON.parse(uploadedData));
+				envStore.set(geojson);
+			};
+
+			const file = files[0] as File;
+			const blob = new Blob([file], { type: file.type });
+
+			reader.readAsText(blob);
+		}
+	};
 </script>
 
 <Editor data={envStore}>
@@ -12,6 +41,9 @@
 		</Widgets>
 	{/snippet}
 	{#snippet actions()}
-		<Button>Button</Button>
+		<Fileupload accept=".geojson" bind:files onUpload={handleUpload}>
+			<span>Upload GeoJSON</span>
+			<UploadIcon class="mt-0.5 h-5 w-5" />
+		</Fileupload>
 	{/snippet}
 </Editor>
