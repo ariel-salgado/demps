ab3591502ffe0e885fc37270fb7f1384c3589853
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2023-12-23 04:09:01 -0300

    Handling feature group events on draw component

--- src/lib/components/leaflet/draw.svelte
+++ src/lib/components/leaflet/draw.svelte
@@ -2,16 +2,18 @@
 	import type { MapContext } from '$lib/components/leaflet';
 
 	import { getContext } from 'svelte';
+	import { coordsToFeature } from '$lib/utils';
 	import { key } from '$lib/components/leaflet';
 
 	import '@geoman-io/leaflet-geoman-free';
 	import '@geoman-io/leaflet-geoman-free/dist/leaflet-geoman.css';
 
-	const { getMap, getFeatureGroup } = getContext<MapContext>(key);
+	const { getMap, getLeaflet, getFeatureGroup, getData } = getContext<MapContext>(key);
 
-	// let L = getLeaflet();
+	let L = getLeaflet();
 	let map = getMap();
 	let featureGroup = getFeatureGroup();
+	let data = getData();
 
 	map.pm.setGlobalOptions({
 		layerGroup: featureGroup
@@ -27,6 +29,25 @@
 	});
 
 	map.pm.setLang('es');
+
+	featureGroup.on('layeradd', ({ layer }) => {
+		// @ts-expect-error - Leaflet typings are messed up
+		const featureCoords = L.GeoJSON.latLngsToCoords(layer._latlngs[0]);
+		const addedFeature = coordsToFeature(featureCoords);
+		data.addFeature(addedFeature);
+	});
+
+	featureGroup.on('layerremove', ({ layer }) => {
+		if ('feature' in layer) {
+			// @ts-expect-error - Leaflet typings are messed up
+			const removedFeatureID = layer.feature.id as number;
+			data.removeFeatureByID(removedFeatureID);
+		} else {
+			// @ts-expect-error - Leaflet typings are messed up
+			const featureCoords = L.GeoJSON.latLngsToCoords(layer._latlngs[0]);
+			data.removeFeatureByCoords(featureCoords);
+		}
+	});
 
 	map.on('pm:create', ({ layer }) => {
 		featureGroup.addLayer(layer);
