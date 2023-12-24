4b82ecc6e6cff48fd6438a9516a39a141a4374ca
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2023-12-24 01:24:11 -0300

    Polymorphic behaviour

--- src/lib/components/ui/button/button.svelte
+++ src/lib/components/ui/button/button.svelte
@@ -4,20 +4,36 @@
 
 	import { cn } from '$lib/utils';
 
-	interface Props extends HTMLAttributes<HTMLButtonElement> {
+	type ButtonOrLinkProps =
+		| (HTMLAttributes<HTMLButtonElement> & { as?: 'button' })
+		| (HTMLAttributes<HTMLAnchorElement> & { as: 'a' });
+
+	type Props = {
 		children: Snippet<void>;
-	}
+	} & ButtonOrLinkProps;
 
 	let { children, class: className, ...props } = $props<Props>();
 </script>
 
-<button
-	type="button"
-	class={cn(
-		'inline-flex h-10 w-full items-center justify-center gap-x-1 whitespace-nowrap rounded-lg bg-indigo-700 px-4 py-2 font-medium text-white ring-offset-white transition-colors hover:bg-indigo-600 focus-visible:bg-indigo-600 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-indigo-600 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
-		className
-	)}
-	{...props}
->
-	{@render children()}
-</button>
+{#if props.as === 'a'}
+	<a
+		class={cn(
+			'inline-flex h-10 w-full items-center justify-center gap-x-1 whitespace-nowrap rounded-lg bg-indigo-700 px-4 py-2 font-medium text-white ring-offset-white transition-colors hover:bg-indigo-600 focus-visible:bg-indigo-600 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-indigo-600 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+			className
+		)}
+		{...props}
+	>
+		{@render children()}
+	</a>
+{:else}
+	<button
+		type="button"
+		class={cn(
+			'inline-flex h-10 w-full items-center justify-center gap-x-1 whitespace-nowrap rounded-lg bg-indigo-700 px-4 py-2 font-medium text-white ring-offset-white transition-colors hover:bg-indigo-600 focus-visible:bg-indigo-600 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-indigo-600 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+			className
+		)}
+		{...props}
+	>
+		{@render children()}
+	</button>
+{/if}
