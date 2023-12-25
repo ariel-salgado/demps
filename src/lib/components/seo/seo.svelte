756fc84e8688df8dcdad5b7c230531977ed66aa2
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2023-12-25 18:51:15 -0300

    Added SEO

---
+++ src/lib/components/seo/seo.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	interface Props {
+		title: string;
+		description: string;
+		children?: Snippet<void>;
+	}
+
+	const { children, title, description } = $props<Props>();
+</script>
+
+<svelte:head>
+	<title>{title}</title>
+	<meta name="description" content={description} />
+	{#if children}
+		{@render children()}
+	{/if}
+</svelte:head>
--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,2 +1,8 @@
+<script lang="ts">
+	import { Seo } from '$lib/components/seo';
+</script>
+
+<Seo title="DEMPS | Inicio" description="DEMPS" />
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
