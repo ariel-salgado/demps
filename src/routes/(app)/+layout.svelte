154aaaa33f1c4552cd9626242c21258d674e61ab
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2024-01-17 12:46:59 -0300

    Added base layout

---
+++ src/routes/(app)/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	const { children } = $props();
+</script>
+
+<div class="grid min-h-screen grid-rows-[auto_1fr_auto]">
+	<header></header>
+
+	<main class="h-full">
+		{@render children()}
+	</main>
+
+	<footer></footer>
+</div>
