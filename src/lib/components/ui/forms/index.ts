a0a5332da6db3ead5613596bb1e180fc475bd5cd
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2024-01-21 22:07:37 -0300

    Added form components

--- src/lib/components/ui/forms/index.ts
+++ src/lib/components/ui/forms/index.ts
@@ -1,7 +1,17 @@
-import Button from './button.svelte';
-import Fileupload from './fileupload.svelte';
+import { Label } from './label';
+import { Input } from './input';
+import { Select } from './select';
+import { Button } from './button';
+import { Formgroup } from './form-group';
+import { Fileupload } from './fileupload';
+import { Description } from './description';
 
 export {
+    Label,
+    Input,
+    Select,
     Button,
+    Formgroup,
     Fileupload,
+    Description
 };
