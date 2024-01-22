42628651f6b2130203c8311e6b34301f5c7ef20b
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2024-01-21 22:07:11 -0300

    Added form group label

---
+++ src/lib/components/ui/forms/label/index.ts
@@ -0,0 +1,5 @@
+import Label from './label.svelte';
+
+export {
+    Label
+}
---
+++ src/lib/components/ui/forms/label/label.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import type { HTMLLabelAttributes } from 'svelte/elements';
+
+	import { cn } from '$lib/utils';
+
+	interface Props extends HTMLLabelAttributes {
+		children: Snippet<void>;
+	}
+
+	const { children, class: className, ...props } = $props<Props>();
+</script>
+
+<label
+	for={undefined}
+	class={cn(
+		'text-base font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70',
+		className
+	)}
+	{...props}
+>
+	{@render children()}
+</label>
