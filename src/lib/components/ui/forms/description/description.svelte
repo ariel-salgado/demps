1d33f3d8e8d47a9565576a876d3603bba7b8ebd7
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2024-01-21 22:06:46 -0300

    Added form group description

---
+++ src/lib/components/ui/forms/description/description.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import type { HTMLAttributes } from 'svelte/elements';
+
+	import { cn } from '$lib/utils';
+
+	interface Props extends HTMLAttributes<HTMLParagraphElement> {
+		children: Snippet<void>;
+	}
+
+	const { children, class: className, ...props } = $props<Props>();
+</script>
+
+<p class={cn('text-sm text-slate-500', className)} {...props}>
+	{@render children()}
+</p>
---
+++ src/lib/components/ui/forms/description/index.ts
@@ -0,0 +1,5 @@
+import Description from './description.svelte';
+
+export {
+    Description
+}
