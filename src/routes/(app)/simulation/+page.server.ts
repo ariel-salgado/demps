3600f65158039c8c86bb3f352c25a873ffceec91
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2024-02-08 23:38:42 -0300

    Added simulation endpoint

--- src/routes/(app)/simulation/+page.server.ts
+++ src/routes/(app)/simulation/+page.server.ts
@@ -1,1 +1,56 @@
+import type { PageServerLoad } from './$types';
+import type { Feature, FeatureCollection, Point } from 'geojson';
+
 export const ssr = false;
+
+const NUM_POINTS = 1000;
+
+export const load: PageServerLoad = async () => {
+	const geoJSON = generateGeoJSON(NUM_POINTS);
+	return geoJSON;
+};
+
+// Mocking
+function generateRandomPoint(polygons: [number, number][][]): Point {
+	const polygon = polygons[Math.floor(Math.random() * polygons.length)];
+	const minX = Math.min(...(polygon as [number, number][]).map((point) => point[0]));
+	const maxX = Math.max(...(polygon as [number, number][]).map((point) => point[0]));
+	const minY = Math.min(...(polygon as [number, number][]).map((point) => point[1]));
+	const maxY = Math.max(...(polygon as [number, number][]).map((point) => point[1]));
+
+	const randomX = Math.random() * (maxX - minX) + minX;
+	const randomY = Math.random() * (maxY - minY) + minY;
+
+	return {
+		type: 'Point',
+		coordinates: [randomX, randomY]
+	};
+}
+
+function generateGeoJSON(numPoints: number): FeatureCollection {
+	// Coordinates of Viña del Mar, Chile
+	const vinadelmarPolygon: [number, number][][] = [
+		[
+			[-71.557988, -33.019315],
+			[-71.547492, -33.003053],
+			[-71.532464, -33.009874],
+			[-71.542959, -33.026134],
+			[-71.557988, -33.019315]
+		]
+	];
+
+	const features: Feature[] = [];
+	for (let i = 0; i < numPoints; i++) {
+		const point = generateRandomPoint(vinadelmarPolygon);
+		features.push({
+			type: 'Feature',
+			properties: {},
+			geometry: point
+		});
+	}
+
+	return {
+		type: 'FeatureCollection',
+		features: features
+	} as FeatureCollection<Point>;
+}
--- src/routes/(app)/simulation/+page.svelte
+++ src/routes/(app)/simulation/+page.svelte
@@ -1,13 +1,23 @@
 <script lang="ts">
+	import type { FeatureCollection, Point } from 'geojson';
+
 	import { simStore } from '$lib/stores';
-	import { Map, Cluster } from '$lib/components/leaflet';
+	import { Map, MarkerCluster } from '$lib/components/leaflet';
+
+	interface Props {
+		data: FeatureCollection<Point>;
+	}
+
+	let { data } = $props<Props>();
+
+	simStore.set(data);
 
 	const zoom: number = 15;
 	const center: [number, number] = [-33.015348, -71.550499];
 </script>
 
 <section class="h-full">
-	<Map store={simStore} {center} {zoom} overlay={false}>
-		<Cluster />
+	<Map {center} {zoom} overlay={false}>
+		<MarkerCluster geojson={simStore} />
 	</Map>
 </section>
