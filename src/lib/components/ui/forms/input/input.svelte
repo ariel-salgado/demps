8126557516ccdb49f2d719d45526517dbfe550d4
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2024-02-17 19:21:28 -0300

    Added validation prop

--- src/lib/components/ui/forms/input/input.svelte
+++ src/lib/components/ui/forms/input/input.svelte
@@ -1,12 +1,30 @@
 <script lang="ts">
+	import type { z } from 'zod';
 	import type { Action } from 'svelte/action';
 	import type { HTMLInputAttributes } from 'svelte/elements';
 
 	import { cn } from '$lib/utils';
 
-	interface Props extends HTMLInputAttributes {}
+	interface Props extends HTMLInputAttributes {
+		validation?: z.ZodType;
+	}
 
-	let { type: type, class: className, value: value, ...props } = $props<Props>();
+	let { type: type, class: className, value: value, validation, ...props } = $props<Props>();
+
+	let validationError: boolean = $state(false);
+	let inputError: string | undefined = $state();
+
+	const validateField = () => {
+		if (!validation) return;
+
+		const result = validation.safeParse(value);
+
+		if (result.success) validationError = false;
+		else {
+			validationError = true;
+			inputError = result.error.format()._errors.at(0);
+		}
+	};
 
 	const initInput: Action<HTMLInputElement> = (element: HTMLInputElement) => {
 		if (type !== 'number') element.type = 'text';
@@ -41,7 +59,19 @@
 		'inline-flex h-9 w-full rounded-md border border-slate-300 px-3 py-2 text-sm ring-offset-white focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary-600 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
 		className
 	)}
+	class:error={validationError}
 	{...props}
 	bind:value
+	on:change={validateField}
 	use:initInput
 />
+
+{#if validationError}
+	<small class="text-red-600">{inputError}</small>
+{/if}
+
+<style lang="postcss">
+	.error {
+		@apply border-2 border-red-600 focus-visible:ring-red-600;
+	}
+</style>
--- src/lib/components/ui/forms/select/select.svelte
+++ src/lib/components/ui/forms/select/select.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import type { z } from 'zod';
+	import type { SelectOptions } from '$lib/types';
 	import type { HTMLSelectAttributes } from 'svelte/elements';
 
 	import { cn } from '$lib/utils';
@@ -6,16 +8,28 @@
 	interface Props extends HTMLSelectAttributes {
 		value?: string | number;
 		placeholder?: string | null;
-		options: Array<{
-			label: string | null;
-			value: string | number | boolean;
-			selected?: boolean | undefined;
-		}>;
+		options: SelectOptions;
+		validation?: z.ZodType;
 	}
 
-	let { value, placeholder, options, class: className, ...props } = $props<Props>();
+	let { value, placeholder, options, class: className, validation, ...props } = $props<Props>();
 
 	const hasDefaultValue = options.some((option) => option.selected === true);
+
+	let validationError: boolean = $state(false);
+	let inputError: string | undefined = $state();
+
+	const validateField = () => {
+		if (!validation) return;
+
+		const result = validation.safeParse(value);
+
+		if (result.success) validationError = false;
+		else {
+			validationError = true;
+			inputError = result.error.format()._errors.at(0);
+		}
+	};
 </script>
 
 <select
@@ -23,7 +37,9 @@
 		'inline-flex h-9 w-full rounded-md border border-slate-300 px-3 py-2 text-sm ring-offset-white focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary-600 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
 		className
 	)}
+	class:error={validationError}
 	bind:value
+	on:change={validateField}
 	{...props}
 >
 	{#if placeholder}
@@ -33,3 +49,13 @@
 		<option {value} {selected}>{label}</option>
 	{/each}
 </select>
+
+{#if validationError}
+	<small class="text-red-600">{inputError}</small>
+{/if}
+
+<style lang="postcss">
+	.error {
+		@apply border-2 border-red-600 focus-visible:ring-red-600;
+	}
+</style>
