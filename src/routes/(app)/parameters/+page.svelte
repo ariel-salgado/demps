706ca523b52b8bf80529584c26a3e74a2c8ef369
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2024-02-18 00:45:16 -0300

    Added side navigation

--- src/routes/(app)/parameters/+page.svelte
+++ src/routes/(app)/parameters/+page.svelte
@@ -1,41 +1,87 @@
 <script lang="ts">
 	import type { FormField } from './form';
 
-	import { flattenForm } from './form';
+	import { getFormData } from './form';
 	import { Description, FormGroup, Input, Label, Select } from '$lib/components/ui/forms/';
 
-	const form = flattenForm();
+	const { form, items } = getFormData();
+
+	let selected: string | undefined = $state();
+
+	const setSelected = (s: string) => (selected = s);
 </script>
 
 {#snippet formField(field: FormField)}
 	<FormGroup>
-		<Label for={field.label}>{field.label}</Label>
+		<Label for={field.attributes.name}>{field.label}</Label>
 		{#if field.type === 'input'}
-			<Input {...field.attributes} validation={field.validation} />
+			<Input id={field.attributes.name} {...field.attributes} validation={field.validation} />
 		{:else if field.type === 'select'}
-			<Select {...field.attributes} options={field.options} validation={field.validation} />
+			<Select
+				id={field.attributes.name}
+				{...field.attributes}
+				options={field.options}
+				validation={field.validation}
+			/>
 		{/if}
 		<Description>{field.description}</Description>
 	</FormGroup>
 {/snippet}
 
 <section class="flex divide-x-2">
-	<aside class="flex-1">
-		<!--  -->
+	<aside
+		class="sticky top-14 grid h-[calc(100vh-3.5rem)] flex-[1] grid-rows-[auto_1fr_auto] gap-y-4 bg-white p-8 shadow-md"
+	>
+		<h1
+			class="scroll-m-20 border-b pb-2 pl-5 text-3xl font-semibold capitalize tracking-tight first:mt-0"
+		>
+			Parametros
+		</h1>
+		<nav>
+			<ul>
+				{#each Object.entries(items) as [key, value]}
+					<li>
+						<a
+							class="block w-full rounded-md px-4 py-2 text-base font-medium capitalize text-slate-500 transition-colors hover:bg-slate-100 hover:text-slate-900"
+							class:active={selected === key}
+							onclick={() => {
+								setSelected(key);
+							}}
+							href={`#${key}`}>{key}</a
+						>
+					</li>
+
+					{#if typeof value === 'object'}
+						{#each Object.keys(value) as subKey}
+							<li>
+								<a
+									class="block w-full rounded-md px-10 py-2 text-base font-medium capitalize text-slate-500 transition-colors hover:bg-slate-100 hover:text-slate-900"
+									class:active={selected === `${key}-${subKey}`}
+									onclick={() => {
+										setSelected(`${key}-${subKey}`);
+									}}
+									href={`#${subKey}`}>{subKey}</a
+								>
+							</li>
+						{/each}
+					{/if}
+				{/each}
+			</ul>
+		</nav>
 	</aside>
-	<form class="grid flex-[3] grid-cols-2 gap-4 p-10" data-sveltekit-keepfocus>
+	<form class="grid flex-[3] grid-cols-2 gap-4 px-10 py-8" data-sveltekit-keepfocus>
 		{#each form as field}
 			{#if 'title' in field}
 				<h2
 					id={field.title}
-					class="col-span-2 scroll-m-24 border-b pb-2 pl-5 text-3xl font-semibold capitalize tracking-tight first:mt-0"
+					class="col-span-2 scroll-m-20 border-b pb-2 pl-5 text-3xl font-semibold capitalize tracking-tight first:mt-0"
 				>
 					{field.title}
 				</h2>
 			{:else if 'subtitle' in field}
 				<h3
 					id={field.subtitle}
-					class="col-span-2 scroll-m-20 pl-5 text-2xl font-semibold capitalize tracking-tight"
+					class="col-span-2 scroll-m-[4.5rem] pl-5 text-2xl font-semibold capitalize tracking-tight"
 				>
 					{field.subtitle}
 				</h3>
@@ -45,3 +91,9 @@
 		{/each}
 	</form>
 </section>
+
+<style lang="postcss">
+	.active {
+		@apply bg-primary-600 text-white hover:bg-primary-700 hover:text-white;
+	}
+</style>
