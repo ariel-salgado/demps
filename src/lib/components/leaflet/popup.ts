145c5d2249b1d72f32ce1a37d5dfcfe8a7e95fab
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2024-02-21 04:11:53 -0300

    Make popup easy to create with fields config variable

--- src/lib/components/leaflet/popup.ts
+++ src/lib/components/leaflet/popup.ts
@@ -1,58 +1,102 @@
+import type { Feature } from 'geojson';
+
+type Field = {
+	id: string;
+	type: string;
+	defaultValue: string | number;
+	attributes: Record<string, unknown>;
+};
+
+const fields: Field[] = [
+	{
+		id: 'id',
+		type: 'text',
+		defaultValue: '',
+		attributes: {
+			readonly: true
+		}
+	},
+	{
+		id: 'nameID',
+		type: 'text',
+		defaultValue: '',
+		attributes: {}
+	},
+	{
+		id: 'zoneType',
+		type: 'text',
+		defaultValue: '',
+		attributes: {}
+	},
+	{
+		id: 'stroke',
+		type: 'color',
+		defaultValue: '#3388ff',
+		attributes: {}
+	},
+	{
+		id: 'stroke-width',
+		type: 'number',
+		defaultValue: 3,
+		attributes: {
+			min: 0
+		}
+	},
+	{
+		id: 'stroke-opacity',
+		type: 'number',
+		defaultValue: 1,
+		attributes: {
+			min: 0,
+			max: 1,
+			step: 0.1
+		}
+	},
+	{
+		id: 'fill',
+		type: 'color',
+		defaultValue: '#3388ff',
+		attributes: {}
+	},
+	{
+		id: 'fill-opacity',
+		type: 'number',
+		defaultValue: 0.2,
+		attributes: {
+			min: 0,
+			max: 1,
+			step: 0.1
+		}
+	}
+];
+
+const formatAttributes = (attributes: Record<string, unknown>) => {
+	return Object.entries(attributes)
+		.map(([key, value]) => `${key}="${value}"`)
+		.join(' ');
+};
+
+const createFormField = (field: Field, feature: Feature) => {
+	const attributes = formatAttributes(field.attributes);
+	const value = field.id === 'id' ? feature.id : feature.properties![field.id];
+
+	return (
+		`<div>` +
+		`<label class="block p-1 font-semibold text-slate-500" for="${field.id}-${feature.id}">${field.id}</label>` +
+		`<input class="w-full px-3 py-1.5 rounded border border-slate-300 read-only:bg-neutral-200 read-only:text-neutral-600" type="${field.type}" id="${field.id}-${feature.id}" name="${field.id}" value="${value || field.defaultValue}" placeholder="Feature ${field.id}" ${attributes}>` +
+		`</div>`
+	);
+};
+
 export const createPopup = (layer: L.Layer) => {
 	return (
 		`<h3 class="text-base font-semibold text-slate-500">Edit Feature</h3>` +
 		// @ts-expect-error - Property 'feature' does not exist on type 'Layer'
 		`<form id="form-${layer.feature.id}" class="w-[300px] grid grid-cols-2 items-center place-content-center gap-4 py-2">` +
-		`<div>` +
 		// @ts-expect-error - Property 'feature' does not exist on type 'Layer'
-		`<label class="block p-1 font-semibold text-slate-500" for="id-${layer.feature.id}">ID</label>` +
-		// @ts-expect-error - Property 'feature' does not exist on type 'Layer'
-		`<input class="bg-neutral-200 text-neutral-500 select-none cursor-not-allowed w-full px-3 py-1.5 rounded border border-slate-300 " type="text" id="id-${layer.feature.id}" name="id" value="${layer.feature.id}" placeholder="Feature ID" readonly>` +
-		`</div>` +
-		`<div>` +
-		// @ts-expect-error - Property 'feature' does not exist on type 'Layer'
-		`<label class="block p-1 font-semibold text-slate-500" for="nameID-${layer.feature.id}">Name ID</label>` +
-		// @ts-expect-error - Property 'feature' does not exist on type 'Layer'
-		`<input class="w-full px-3 py-1.5 rounded border border-slate-300" type="text" id="nameID-${layer.feature.id}" name="nameID" value="${layer.feature.properties.nameID || ''}" placeholder="Feature name ID">` +
-		`</div>` +
-		`<div>` +
-		// @ts-expect-error - Property 'feature' does not exist on type 'Layer'
-		`<label class="block p-1 font-semibold text-slate-500" for="zoneType-${layer.feature.id}">Zone Type</label>` +
-		// @ts-expect-error - Property 'feature' does not exist on type 'Layer'
-		`<input class="w-full px-3 py-1.5 rounded border border-slate-300" type="text" id="zoneType-${layer.feature.id}" name="zoneType" value="${layer.feature.properties.zoneType || ''}" placeholder="Feature zone type">` +
-		`</div>` +
-		`<div>` +
-		// @ts-expect-error - Property 'feature' does not exist on type 'Layer'
-		`<label class="block p-1 font-semibold text-slate-500" for="stroke-${layer.feature.id}">Stroke</label>` +
-		// @ts-expect-error - Property 'feature' does not exist on type 'Layer'
-		`<input class="w-full px-3 py-1.5 rounded border border-slate-300" type="color" id="stroke-${layer.feature.id}" name="stroke" value="${layer.feature.properties.stroke || '#3388ff'}">` +
-		`</div>` +
-		`<div>` +
-		// @ts-expect-error - Property 'feature' does not exist on type 'Layer'
-		`<label class="block p-1 font-semibold text-slate-500" for="stroke-width-${layer.feature.id}">Stroke Width</label>` +
-		// @ts-expect-error - Property 'feature' does not exist on type 'Layer'
-		`<input class="w-full px-3 py-1.5 rounded border border-slate-300" type="number" id="stroke-width-${layer.feature.id}" name="stroke-width" value="${layer.feature.properties['stroke-width'] || 3}" placeholder="Feature stroke width">` +
-		`</div>` +
-		`<div>` +
-		// @ts-expect-error - Property 'feature' does not exist on type 'Layer'
-		`<label class="block p-1 font-semibold text-slate-500" for="stroke-opacity-${layer.feature.id}">Stroke Opacity</label>` +
-		// @ts-expect-error - Property 'feature' does not exist on type 'Layer'
-		`<input class="w-full px-3 py-1.5 rounded border border-slate-300" type="number" id="stroke-opacity-${layer.feature.id}" name="stroke-opacity" value="${layer.feature.properties['stroke-opacity'] || 1}" min="0" max="1" step="0.1" placeholder="Feature stroke opacity">` +
-		`</div>` +
-		`<div>` +
-		// @ts-expect-error - Property 'feature' does not exist on type 'Layer'
-		`<label class="block p-1 font-semibold text-slate-500" for="fill-${layer.feature.id}">Fill</label>` +
-		// @ts-expect-error - Property 'feature' does not exist on type 'Layer'
-		`<input class="w-full px-3 py-1.5 rounded border border-slate-300" type="color" id="fill-${layer.feature.id}" name="fill" value="${layer.feature.properties.fill || '#3388ff'}">` +
-		`</div>` +
-		`<div>` +
-		// @ts-expect-error - Property 'feature' does not exist on type 'Layer'
-		`<label class="block p-1 font-semibold text-slate-500" for="fill-opacity-${layer.feature.id}">Fill Opacity</label>` +
-		// @ts-expect-error - Property 'feature' does not exist on type 'Layer'
-		`<input class="w-full px-3 py-1.5 rounded border border-slate-300" type="number" id="fill-opacity-${layer.feature.id}" name="fill-opacity" value="${layer.feature.properties['fill-opacity'] || 0.2}" min="0" max="1" step="0.1" placeholder="Feature fill opacity">` +
-		`</div>` +
+		fields.map((field) => createFormField(field, layer.feature)).join('') +
 		`<hr class="col-span-2" />` +
-		`<button class="w-full px-3 py-1.5 text-white bg-primary-600 rounded hover:bg-primary-700 col-span-2">Save</button>` +
+		`<button class="w-full px-3 py-1.5 text-white bg-primary-700 rounded hover:bg-primary-600 col-span-2">Save</button>` +
 		`</form>`
 	);
 };
