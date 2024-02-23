70f9a88f62500cc57af7d2720d2a3ef14fdff28d
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2024-02-23 05:27:18 -0300

    Added red icon for markers

--- src/lib/components/leaflet/markercluster.svelte
+++ src/lib/components/leaflet/markercluster.svelte
@@ -6,6 +6,9 @@
 	import { getContext } from 'svelte';
 	import { contextKey } from '$lib/components/leaflet';
 
+	import MarkerIcon from '$lib/assets/images/marker-icon-2x-red.png';
+	import MarkerShadow from '$lib/assets/images/marker-shadow.png';
+
 	import 'leaflet.markercluster';
 	import 'leaflet.markercluster/dist/MarkerCluster.css';
 	import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
@@ -24,6 +27,15 @@
 
 	const usingMapStore = geojson === undefined;
 	const store = geojson || mapStore;
+
+	const customMarker = L.icon({
+		iconUrl: MarkerIcon,
+		shadowUrl: MarkerShadow,
+		iconSize: [25, 41],
+		iconAnchor: [12, 41],
+		popupAnchor: [1, -34],
+		shadowSize: [41, 41]
+	});
 
 	// @ts-expect-error - markerClusterGroup is not part of the leaflet types
 	let markers = window.L.markerClusterGroup({
@@ -45,10 +57,10 @@
 
 			geojson.features.forEach((feature) => {
 				if (feature.geometry.type === 'Point') {
-					const marker = L.marker([
-						feature.geometry.coordinates[1]!,
-						feature.geometry.coordinates[0]!
-					]);
+					const marker = L.marker(
+						[feature.geometry.coordinates[1]!, feature.geometry.coordinates[0]!],
+						{ icon: customMarker }
+					);
 					markers.addLayer(marker);
 				}
 			});
