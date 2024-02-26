11e938289d13da0820ea654a9a93f37a49dfb23a
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2024-02-26 00:08:51 -0300

    Added csp directives

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,15 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
 			precompress: true,
-		})
+		}),
+
+		csp: {
+			directives: {
+				"script-src": ["'self'", "'unsafe-inline'"],
+				"default-src": ['self'],
+				"base-uri": ['self'],
+			}
+		}
 	},
 
 	compilerOptions: {
