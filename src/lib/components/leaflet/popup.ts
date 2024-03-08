b54f35368b451a8eae82c4137c6bb8c55f04dfed
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2024-03-08 02:05:09 -0300

    Changed zoneType field to select type

--- src/lib/components/leaflet/popup.ts
+++ src/lib/components/leaflet/popup.ts
@@ -5,68 +5,35 @@ type Field = {
 	type: string;
 	defaultValue: string | number;
 	attributes: Record<string, unknown>;
+	options?: string[];
+	placeholder?: string;
 };
 
 const fields: Field[] = [
-	{
-		id: 'id',
-		type: 'text',
-		defaultValue: '',
-		attributes: {
-			readonly: true
-		}
-	},
-	{
-		id: 'nameID',
-		type: 'text',
-		defaultValue: '',
-		attributes: {}
-	},
+	{ id: 'id', type: 'text', defaultValue: '', attributes: { readonly: true } },
+	{ id: 'nameID', type: 'text', defaultValue: '', attributes: {} },
 	{
 		id: 'zoneType',
-		type: 'text',
+		type: 'select',
 		defaultValue: '',
-		attributes: {}
+		attributes: {},
+		options: ['initial', 'flood', 'safe'],
+		placeholder: 'Select zone type'
 	},
-	{
-		id: 'stroke',
-		type: 'color',
-		defaultValue: '#3388ff',
-		attributes: {}
-	},
-	{
-		id: 'stroke-width',
-		type: 'number',
-		defaultValue: 3,
-		attributes: {
-			min: 0
-		}
-	},
+	{ id: 'stroke', type: 'color', defaultValue: '#3388ff', attributes: {} },
+	{ id: 'stroke-width', type: 'number', defaultValue: 3, attributes: { min: 0 } },
 	{
 		id: 'stroke-opacity',
 		type: 'number',
 		defaultValue: 1,
-		attributes: {
-			min: 0,
-			max: 1,
-			step: 0.1
-		}
+		attributes: { min: 0, max: 1, step: 0.1 }
 	},
-	{
-		id: 'fill',
-		type: 'color',
-		defaultValue: '#3388ff',
-		attributes: {}
-	},
+	{ id: 'fill', type: 'color', defaultValue: '#3388ff', attributes: {} },
 	{
 		id: 'fill-opacity',
 		type: 'number',
 		defaultValue: 0.2,
-		attributes: {
-			min: 0,
-			max: 1,
-			step: 0.1
-		}
+		attributes: { min: 0, max: 1, step: 0.1 }
 	}
 ];
 
@@ -79,13 +46,39 @@ const formatAttributes = (attributes: Record<string, unknown>) => {
 const createFormField = (field: Field, feature: Feature) => {
 	const attributes = formatAttributes(field.attributes);
 	const value = field.id === 'id' ? feature.id : feature.properties![field.id];
+	const defaultValue =
+		field.id === 'zoneType'
+			? feature.properties![field.id] || field.placeholder
+			: value || field.defaultValue;
+	const isPlaceholderSelected =
+		field.id === 'zoneType' && !field.options?.includes(defaultValue as string);
 
-	return (
-		`<div>` +
-		`<label class="block p-1 font-semibold text-slate-500" for="${field.id}-${feature.id}">${field.id}</label>` +
-		`<input class="w-full px-3 py-1.5 rounded border border-slate-300 read-only:bg-neutral-200 read-only:text-neutral-600" type="${field.type}" id="${field.id}-${feature.id}" name="${field.id}" value="${value || field.defaultValue}" placeholder="Feature ${field.id}" ${attributes}>` +
-		`</div>`
-	);
+	switch (field.type) {
+		case 'select':
+			return (
+				`<div>` +
+				`<label class="block p-1 font-semibold text-slate-500" for="${field.id}-${feature.id}">${field.id}</label>` +
+				`<select class="w-full px-2 py-1.5 rounded border border-slate-300 ${isPlaceholderSelected ? 'text-slate-500' : ''}" id="${field.id}-${feature.id}" name="${field.id}" ${attributes}>` +
+				`<option value="" disabled ${isPlaceholderSelected ? 'selected' : ''}>` +
+				`${field.placeholder}` +
+				`</option>` +
+				(field.options || [])
+					.map(
+						(option) =>
+							`<option value="${option}" ${defaultValue === option ? 'selected' : ''}>${option}</option>`
+					)
+					.join('') +
+				`</select>` +
+				`</div>`
+			);
+		default:
+			return (
+				`<div>` +
+				`<label class="block p-1 font-semibold text-slate-500" for="${field.id}-${feature.id}">${field.id}</label>` +
+				`<input class="w-full px-3 py-1.5 rounded border border-slate-300 read-only:bg-neutral-200 read-only:text-neutral-600" type="${field.type}" id="${field.id}-${feature.id}" name="${field.id}" value="${defaultValue}" placeholder="Feature ${field.id}" ${attributes}>` +
+				`</div>`
+			);
+	}
 };
 
 export const createPopup = (layer: L.Layer) => {
