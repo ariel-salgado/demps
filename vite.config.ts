3b3d80017a0239a071269ff47da2d06dd8437f0f
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2024-03-12 13:21:17 -0300

    Added vitest for testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
+import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
-import type { UserConfig } from 'vite';
 
-const config: UserConfig = {
-	plugins: [sveltekit()]
-};
-
-export default config;
+export default defineConfig({
+	plugins: [sveltekit()],
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	}
+});
