9c0d17ab2633d9e543d1a6c675658f92e5c642bc
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2024-04-18 19:27:44 -0400

    Changed style and position of searcher

--- src/lib/components/leaflet/search.svelte
+++ src/lib/components/leaflet/search.svelte
@@ -15,7 +15,7 @@
 		// @ts-expect-error - no types for leaflet-geosearch
 		const search = new GeoSearchControl({
 			provider: new OpenStreetMapProvider(),
-			style: 'button',
+			style: 'bar',
 			autoClose: true,
 			showMarker: false,
 			searchLabel: 'Buscar una dirección',
@@ -25,5 +25,8 @@
 		map.addControl(search);
 
 		document.querySelector('.leaflet-geosearch-button form input')?.setAttribute('name', 'search');
+		document
+			.querySelector('.leaflet-control-container')
+			?.lastElementChild?.classList.add('!absolute', 'top-0', 'left-14', '!max-w-[300px]');
 	});
 </script>
